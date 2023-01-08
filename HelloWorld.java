90cb5803085888d865da9e88bb0a00d6b7c2293f
Nayara Cristina Jesus <122224446+nayara-cristina-jesus@users.noreply.github.com>
2023-01-08 15:44:40 -0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+Autor: Nayara Cristina Jesus
+Date: 08-01-2023
+*/
+
+// Hello World in Java
+
+class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
